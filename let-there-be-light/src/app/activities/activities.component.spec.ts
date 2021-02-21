4cfe20472ba468e6eccda37646bc02ff59731440
ggs5427 <ggs5427@rit.edu>
2021-02-21 04:26:01 -0500

    activities page

--- let-there-be-light/src/app/activities/activities.component.spec.ts
+++ let-there-be-light/src/app/activities/activities.component.spec.ts
@@ -1,17 +1,22 @@
 import { createComponentFactory, mockProvider, Spectator } from '@ngneat/spectator';
 import { MockComponents, MockDirectives } from 'ng-mocks';
 import { ActivitiesComponent } from './activities.component';
-import {MatSelectModule} from '@angular/material/select';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDialog } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
 
 describe('ActivitiesComponent', () => {
   let spectator: Spectator<ActivitiesComponent>;
   const createComponent = createComponentFactory({
     component: ActivitiesComponent,
-    providers: [],
+    providers: [mockProvider(MatDialog)],
     imports: [
       MatSelectModule,
       MatFormFieldModule,
+      MatInputModule,
+      ReactiveFormsModule
     ]
     
   });
