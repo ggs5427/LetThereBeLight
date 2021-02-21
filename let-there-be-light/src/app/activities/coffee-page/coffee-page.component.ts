b1d2dea85adc25d17eea5398a01b6024ead1a6c0
ggs5427 <ggs5427@rit.edu>
2021-02-20 20:53:29 -0500

    angular setup

---
+++ let-there-be-light/src/app/activities/coffee-page/coffee-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-coffee-page',
+  templateUrl: './coffee-page.component.html',
+  styleUrls: ['./coffee-page.component.scss']
+})
+export class CoffeePageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- let-there-be-light/src/app/app.module.ts
+++ let-there-be-light/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoffeePageComponent } from './coffee-page/coffee-page.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoffeePageComponent,
+    ContactUsComponent
   ],
   imports: [
     BrowserModule,
