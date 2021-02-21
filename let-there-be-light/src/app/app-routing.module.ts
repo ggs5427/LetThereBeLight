ccf7aa469e83686f65fb2309cd5e019a640b0caa
Saakshi <sd3464@rit.edu>
2021-02-21 06:02:19 -0500

    idk

--- let-there-be-light/src/app/app-routing.module.ts
+++ let-there-be-light/src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ActivitiesComponent } from './activities/activities.component';
+
 const routes: Routes = [
+
     {
       path:'activities', 
       data: {title:''}, 
+      //loadChildren: () => import ('./activities/activities.module').then((m) => m.ActivitiesModule)
       component: ActivitiesComponent
     }
 ];
